e27f2cbddb5ccc6a7a1396a52f35f9844608dd6b
Romain Commandé <commande.romain@gmail.com>
2015-07-12 20:28:07 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -8,7 +8,15 @@ setup(
     description="",
     long_description="""\
 """,
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='decorator',
     author='Romain Commandé',
     author_email='commande.romain+decorrelate@gmail.com',
