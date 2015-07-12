d3fb24f94f5081bb2f306dfe47b696917c48a112
Romain Commandé <commande.romain@gmail.com>
2015-07-12 20:30:01 +0200

    Drop python 3.0 and 3.1 support

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
